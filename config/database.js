6e1c82e2546d3ce86d01790d85180f2a526b20d7
Hakimuddin <hakimuddin2152@gmail.com>
2018-02-11 12:43:22 +0530

    Test meanauth app on heroku

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // configure the code below with your username, password and mlab database information
+  database: 'mongodb://hakim:hakim123@ds12226.mlab.com:31658/ecommerce-app',
+  //database: 'mongodb://localhost:27017/meanauth',    //dev
+  secret: 'yoursecret'
+}
