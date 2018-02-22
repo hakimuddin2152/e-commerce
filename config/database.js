767ce6005b2843d9ae5871b5eff0ea1c02d78451
Hakimuddin <hakimuddin2152@gmail.com>
2018-02-22 23:38:23 +0530

    Ecommerce app config update

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 module.exports = {
   // configure the code below with your username, password and mlab database information
-  database: 'mongodb://hakim:hakim123@ds12226.mlab.com:31658/ecommerce-app',
+  database: 'mongodb://hakim:hakim123@ds145208.mlab.com:45208/productdb-shoppingmart',
+  //mongodb://<dbuser>:<dbpassword>@ds145208.mlab.com:45208/productdb-shoppingmart
   //database: 'mongodb://localhost:27017/meanauth',    //dev
   secret: 'yoursecret'
 }
